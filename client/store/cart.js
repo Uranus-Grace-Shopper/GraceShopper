a4dd954cf07b75e959ad5f86a1ac7fa1bb216c0c
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-03-03 16:25:10 -0500

    added redux store

---
+++ client/store/cart.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+//action type
+const SET_CART = "SET_CART";
+
+//action creator- fetch all products
+const setCart = (cart) => {
+  return {
+    type: SET_CART,
+    cart,
+  };
+};
+
+//fetch individual cart thunk
+export const fetchCart = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data: cart } = await axios.get(`/api/cart/${id}`);
+      dispatch(setCart(cart));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+const initialState = [];
+
+//reducer
+
+export default function cartReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_CART:
+      return action.cart;
+    default:
+      return state;
+  }
+}
