4ecc27ec814d1e2b3adb46dc5b4a4fb21804049d
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-03-03 23:49:29 -0400

    Updated cart component to have a checkout button and qty input area
    - Attemped to have a warning to show up when itemQty > stockQty using local state but not working
    - Updated CSS

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -4,9 +4,46 @@ import { fetchCart } from "../store/cart";
 import { Link } from "react-router-dom";
 
 class Cart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cartItemQty: 1,
+      cartItemTotal: null,
+      QtyError: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.validateQty = this.validateQty.bind(this);
+  }
+
   componentDidMount() {
     this.props.getCart(this.props.match.params.id);
   }
+
+  //needs an update
+  handleChange(event) {
+    if (this.validateQty()) {
+      this.setState({
+        [event.target.name]: event.target.value,
+      });
+      console.log(this.state);
+    }
+  }
+
+  //needs an update
+  validateQty() {
+    let QtyError = "";
+    if (this.state.cartItemQty > 20) {
+      //needs an update so it can read each products stockQty
+      QtyError = "Not enough bottles in stock.";
+    }
+    if (QtyError) {
+      event.preventDefault();
+      this.setState({ QtyError });
+      return false;
+    }
+    return true;
+  }
+
   render() {
     const cartItems = this.props.cart.products || [];
     console.log("cartItems >>>>>>>>>>>", cartItems);
@@ -30,15 +67,19 @@ class Cart extends React.Component {
                 </h4>
               </Link>
               <button className="btn-cart-qty">+</button>
-              <ul>qty</ul>
+              <input
+                name="cartItemQty"
+                onChange={this.handleChange}
+                value={this.state.cartItemQty}
+              />
               <button className="btn-cart-qty">-</button>
               <button className="btn-cart-delete">Delete</button>
               <ul>{cartItem.price}</ul>
-              <ul>{cartItem.price * 3}</ul>
+              <ul>{cartItem.price * this.state.cartItemQty}</ul>
             </div>
           ))}
         </div>
-        <button>checkout</button>
+        <button className="btn-large">CHECKOUT</button>
       </div>
     );
   }
