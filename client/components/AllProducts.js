ec4916e0c190fd77dd278d4e31dc41cd7862c1e3
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-03-07 17:40:24 -0400

    filtering red/white functionality is added but CSS is outstanding

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -8,8 +8,8 @@ class AllProducts extends React.Component {
   constructor() {
     super();
     this.state = {
-      variety: '' 
-    }
+      variety: "",
+    };
     this.findOneWine = this.findOneWine.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
@@ -41,16 +41,20 @@ class AllProducts extends React.Component {
     }
   }
 
-  handleChange (evt) {
-    if (evt.target.value === 'red') {
-      this.setState({variety: evt.target.value})
-      console.log(this.state.variety)
-    } else if (evt.target.value === 'white') {
-      this.setState({variety: evt.target.value})
-    } else {
-      this.setState({variety: ''})
-    }
-    return this.state.variety
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+
+    // if (evt.target.value === 'red') {
+    //   this.setState({variety: evt.target.value})
+    //   console.log(this.state.variety)
+    // } else if (evt.target.value === 'white') {
+    //   this.setState({variety: evt.target.value})
+    // } else {
+    //   this.setState({variety: ''})
+    // }
+    // return this.state.variety
   }
 
   componentDidMount() {
@@ -58,35 +62,71 @@ class AllProducts extends React.Component {
   }
   render() {
     const products = this.props.allProducts;
+    console.log("this is variety >>>>>>> ", this.state.variety);
+
     return (
       <div>
         <label>Select wine type:</label>
-        <select>
-          <option value="red" onClick={() => this.handleChange}>Red</option>
-          <option value="white" onClick={() => this.handleChange}>White</option>
+
+        <select
+          value={this.state.variety}
+          name="variety"
+          onChange={this.handleChange}
+        >
+          <option />
+          <option>red</option>
+          <option>white</option>
         </select>
-        
+
         <div className="div-all-products">
           {products.map((product) => (
             <div key={product.id} className="div-each-product">
-              <img className="img-all-products" src={product.imageURL} />
-              <div className="txt-each-product">
+              {this.state.variety === product.variety ? (
                 <div>
-                  <Winery winery={product.winery} />
+                  <img className="img-all-products" src={product.imageURL} />
+                  <div className="txt-each-product">
+                    <div>
+                      <Winery winery={product.winery} />
+                    </div>
+                    <Link to={`/products/${product.id}`}>
+                      <p className="product-name">
+                        {product.name} {product.year}
+                      </p>
+                    </Link>
+                  </div>
+                  <button
+                    onClick={() => this.findOneWine(product)}
+                    className="btn-large"
+                    type="submit"
+                  >
+                    ADD TO CART
+                  </button>
                 </div>
-                <Link to={`/products/${product.id}`}>
-                  <p className="product-name">
-                    {product.name} {product.year}
-                  </p>
-                </Link>
-              </div>
-                <button
-                  onClick={() => this.findOneWine(product)}
-                  className="btn-large"
-                  type="submit"
-                >
-                  ADD TO CART
-                </button>
+              ) : this.state.variety === "" ? (
+                <div>
+                  {" "}
+                  <img className="img-all-products" src={product.imageURL} />
+                  <div className="txt-each-product">
+                    <div>
+                      <Winery winery={product.winery} />
+                    </div>
+                    <Link to={`/products/${product.id}`}>
+                      <p className="product-name">
+                        {product.name} {product.year}
+                      </p>
+                    </Link>
+                  </div>
+                  <button
+                    onClick={() => this.findOneWine(product)}
+                    className="btn-large"
+                    type="submit"
+                  >
+                    ADD TO CART
+                  </button>{" "}
+                </div>
+              ) : (
+                <div></div>
+              )}
             </div>
           ))}
         </div>
