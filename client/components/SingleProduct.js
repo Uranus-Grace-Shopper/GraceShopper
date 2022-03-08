6dd163ef63533aa28877b4ffaff2a2d6a32986c6
Lei Shi <leishi567@gmail.com>
2022-03-07 20:41:43 -0500

    add button on single product page only allows logged in user to add one kind of wine

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -6,24 +6,22 @@ import { addingProductsToCart } from "../store/cart";
 class SingleProduct extends React.Component {
   constructor() {
     super();
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.findOneWine = this.findOneWine.bind(this);
   }
-  handleSubmit(evt) {
-    evt.preventDefault();
+  findOneWine(wine) {
+    //evt.preventDefault();
     //guest user
+    const cart = this.props.cart
     const userInfo = this.props.userInfo;
-    let chosenWine = this.props.singleProduct;
-    let chosenWineId = this.props.singleProduct.id;
     if (Object.keys(userInfo).length === 0 && userInfo.constructor === Object) {
-      //if there is a cart
       let wineInCart = JSON.parse(localStorage.getItem("Cart"));
       console.log("wine in the cart >>>>> ", wineInCart);
       if (!wineInCart) {
-        localStorage.setItem("Cart", JSON.stringify([chosenWine]));
+        localStorage.setItem("Cart", JSON.stringify([wine]));
       } else {
         let isWineAlreadyInCart = false;
-        wineInCart.forEach((wine) => {
-          if (wine.id === chosenWineId) {
+        wineInCart.forEach((product) => {
+          if (product.id === wine.id) {
             alert(
               "This wine is already in the cart. \nUpdate the quantity in your cart page."
             );
@@ -31,15 +29,25 @@ class SingleProduct extends React.Component {
           }
         });
         if (!isWineAlreadyInCart) {
-          wineInCart.push(chosenWine);
+          wineInCart.push(wine);
           localStorage.setItem("Cart", JSON.stringify(wineInCart));
         }
       }
     } else {
-      this.props.addingProductsToCart(chosenWineId);
+      let isWineAlreadyInCart = false;
+      cart.forEach((product) => {
+        if (product.id === wine.id) {
+          alert(
+            "This wine is already in the cart. \nUpdate the quantity in your cart page."
+          );
+          isWineAlreadyInCart = true;
+        }
+      });
+      if (!isWineAlreadyInCart) {
+        this.props.addingProductsToCart(wine.id);
+      }
     }
   }
-
   componentDidMount() {
     //this.props.loadInitialData();
     const productId = this.props.match.params.productId;
@@ -51,36 +59,33 @@ class SingleProduct extends React.Component {
     const product = this.props.singleProduct;
     return (
       <div className="single-product">
-       
-          <div className="product-details">
-            <img className="img-wine" src={product.imageURL} />
-            <table>
-              <tbody>
-                <tr>
-                  <td>Name:</td>
-                  <td>{product.name}</td>
-                </tr>
-                <tr>
-                  <td>Vintage:</td>
-                  <td>{product.year}</td>
-                </tr>
-                <tr>
-                  <td>Price:</td>
-                  <td>${product.price}</td>
-                </tr>
-                <tr>
-                  <td>Description:</td>
-                  <td>{product.description}</td>
-                </tr>
-              </tbody>
-            </table>
-            <form onSubmit={this.handleSubmit}>
-            <button className="btn-large" type="submit">
+        <div className="product-details">
+          <img className="img-wine" src={product.imageURL} />
+          <table>
+            <tbody>
+              <tr>
+                <td>Name:</td>
+                <td>{product.name}</td>
+              </tr>
+              <tr>
+                <td>Vintage:</td>
+                <td>{product.year}</td>
+              </tr>
+              <tr>
+                <td>Price:</td>
+                <td>${product.price}</td>
+              </tr>
+              <tr>
+                <td>Description:</td>
+                <td>{product.description}</td>
+              </tr>
+            </tbody>
+          </table>
+            <button className="btn-large" type="submit"
+            onClick = {()=>this.findOneWine(product)}>
               ADD TO CART
             </button>
-            </form>
-          </div>
-        
+        </div>
       </div>
     );
   }
@@ -91,6 +96,7 @@ const mapState = (state) => {
   return {
     singleProduct: state.singleProduct,
     userInfo: state.auth,
+    cart: state.cart
   };
 };
 
